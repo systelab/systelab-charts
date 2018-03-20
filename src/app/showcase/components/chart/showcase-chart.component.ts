d875c09ddc6c776e65e7c36cde6fee0cb74a0e20
Aritz <apenalver@werfen.com>
2018-03-20 17:59:51 +0100

    Tooltips configuration and Update data

--- src/app/showcase/components/chart/showcase-chart.component.ts
+++ src/app/showcase/components/chart/showcase-chart.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ChartItem, ChartLineAnnotation, ChartBoxAnnotation, ChartLabelAnnotation } from '../../../systelab-charts/chart/chart.component';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ChartComponent, ChartItem, ChartLineAnnotation, ChartBoxAnnotation, ChartLabelAnnotation, ChartTooltipItem, ChartTooltipSettings } from '../../../systelab-charts/chart/chart.component';
 
 @Component({
 	selector: 'showcase-chart',
@@ -30,12 +30,22 @@ export class ShowcaseChartComponent {
 	public legendOff = false;
 	public xLabelAxis = 'Title X';
 	public yLabelAxis = 'Title Y';
+	public chartTooltipSettings = new ChartTooltipSettings();
+	@ViewChild('lineChart') lineChart: ChartComponent;
 	constructor() {
 		this.typeChart = 'line';
 		this.legend = true;
 		this.labels = ['January', 'February', 'March', 'April'];
 
-		this.dataLine.push(new ChartItem('Only Line', [13, 20, 21, 15], '', '', false, true, false, 3));
+		this.chartTooltipSettings.backgroundColor = '#ffffff';
+		this.chartTooltipSettings.borderColor = '#0066ff';
+		this.chartTooltipSettings.borderWidth = 3;
+		this.chartTooltipSettings.bodyFontColor = '#6c757d';
+		this.chartTooltipSettings.titleFontColor = '#fd7e14';
+
+		this.dataLine.push(new ChartItem('Only Line', [13, 20, 21, 15], '', '', false, true, false, 3, '',
+			new ChartTooltipItem('title', 'label', 'afterlabel', true)));
+
 		this.dataLine.push(new ChartItem('Only Dots', [11, 18, 4, 3], '', '', false, false, false, 4));
 		this.dataLine.push(new ChartItem('Line and Area', [12, 41, 1, 21], '', '', true, true, false, 3));
 
@@ -51,7 +61,7 @@ export class ShowcaseChartComponent {
 
 		this.dataPolarArea.push(new ChartItem('', [21, 23, 42, 52], '', '', true, true, false, 3));
 
-		this.dataBubble.push(new ChartItem('Test 1', [{ x: 13, y: 13, r: 4 }, { x: 1, y: 2, r: 3 }, { x: 15, y: 23, r: 4 },
+		this.dataBubble.push(new ChartItem('Test 1', [{ x: 13, y: 13, r: 4, t: 'Tooltip' }, { x: 1, y: 2, r: 3 }, { x: 15, y: 23, r: 4 },
 		{ x: -2, y: -2, r: 4 }, { x: -10, y: 13, r: 3 }, { x: 23, y: 12, r: 7 }, { x: 4, y: 4, r: 8 },
 		{ x: 3, y: 2, r: 9 }], '', '', true, false, false, 2));
 		this.dataBubble.push(new ChartItem('Test 2', [{ x: 6, y: -2, r: 4 }, { x: 2, y: 5, r: 3 }, { x: 12, y: 11, r: 4 }, { x: 5, y: 10, r: 4 },
@@ -88,5 +98,25 @@ export class ShowcaseChartComponent {
 		const yValue = this.dataPie[0].data[this.itemSelected._index];
 		console.log(xValue);
 		console.log(yValue);
+	}
+
+	public doChange() {
+		this.dataLine = [];
+		const rnd = this.randomIntFromInterval(1, 4);
+		for (let h = 1; h <= rnd; h++) {
+			const dataRnd = [];
+			for (let i = 1; i <= 4; i++) {
+				dataRnd.push(this.randomIntFromInterval(3, 35));
+			}
+			let fill = false;
+			if (this.randomIntFromInterval(0, 1) === 1) {
+				fill = true;
+			}
+			this.dataLine.push(new ChartItem('Line ' + h, dataRnd, '', '', fill, true, false, 3));
+		}
+		this.lineChart.doUpdate();
+	}
+	private randomIntFromInterval(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
 	}
 }
