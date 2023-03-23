9f4de7a9639202dcdacaf1c924e1baaec217e6bc
Francisco Javier Aguado Joaquín <github@fjaguado.com>
2023-03-23 18:53:24 +0100

    Update ChartJS library

--- projects/showcase/src/app/components/chart/showcase-chart.component.ts
+++ projects/showcase/src/app/components/chart/showcase-chart.component.ts
@@ -3,16 +3,15 @@ import { Component, ViewChild } from '@angular/core';
 import {
 	Annotation, ChartBoxAnnotation, ChartComponent, ChartItem, ChartLabelAnnotation, ChartLabelColor,
 	ChartLabelFont, ChartLabelPadding, ChartLabelPosition, ChartLabelSettings, ChartLineAnnotation, ChartMultipleYAxisScales,
-	ChartTooltipItem, ChartTooltipSettings, ChartMeterConfiguration, ChartLine
+	ChartTooltipItem, ChartTooltipSettings, ChartLine
 } from 'systelab-charts';
 
 @Component({
-	selector:    'showcase-chart',
+	selector:    'app-showcase-chart',
 	templateUrl: './showcase-chart.component.html'
 })
 export class ShowcaseChartComponent {
 
-	@ViewChild('radialMeterChart') radialMeterChart: ChartComponent;
 	@ViewChild('digitalMeterChart') digitalMeterChart: ChartComponent;
 	@ViewChild('horizontalLinearMeterChart') horizontalLinearMeterChart: ChartComponent;
 	@ViewChild('verticalLinearMeterChart') verticalLinearMeterChart: ChartComponent;
@@ -29,10 +28,6 @@ export class ShowcaseChartComponent {
 	public dataTimeLine: Array<ChartItem> = [];
 	public dataLineMultipleAxis: Array<ChartItem> = [];
 	public dataBar: Array<ChartItem> = [];
-	public radialDataChartMeterGadget: Array<ChartItem> = [];
-	public verticalLinearDataChartMeterGadget: Array<ChartItem> = [];
-	public horizontalLinearDataChartMeterGadget: Array<ChartItem> = [];
-	public digitalDataChartMeterGadget: Array<ChartItem> = [];
 	public dataRadar: Array<ChartItem> = [];
 	public dataPie: Array<ChartItem> = [];
 	public dataDoughnut: Array<ChartItem> = [];
@@ -47,8 +42,9 @@ export class ShowcaseChartComponent {
 	public chartMultipleAnnotations: Array<Annotation> = [];
 	public chartBubbleAnnotations: Array<Annotation> = [];
 	public labels: Array<string> = [];
+	public labelsTimeLine: Array<Date> = [];
 	public labelsChartMeterGadget: Array<string> = [];
-	public labelsMultipleAxis: Array<number> = [];
+	public labelsMultipleAxis: Array<string> = [];
 	public labelLineAnnotations: Array<number> = [];
 	public isBackgroundGrid = false;
 	public yMinValue = 0;
@@ -61,24 +57,16 @@ export class ShowcaseChartComponent {
 	public dataLineCustomLegend: Array<ChartItem> = [];
 	public multipleYAxisScales: Array<ChartMultipleYAxisScales> = [];
 	public pieChartLabelSettings: ChartLabelSettings;
-	public radialChartGadgetConfiguration: ChartMeterConfiguration;
-	public verticalLinearChartGadgetConfiguration: ChartMeterConfiguration;
-	public horizontalLinearChartGadgetConfiguration: ChartMeterConfiguration;
-	public digitalChartGadgetConfiguration: ChartMeterConfiguration;
 	public defaultResizeWidth = 1280;
 	public defaultResizeHeight = 900;
 	public resizedImage: string;
 	public chartLine: ChartLine;
 
-	private static randomIntFromInterval(min, max) {
-		return Math.floor(Math.random() * (max - min + 1) + min);
-	}
-
 	constructor() {
 		this.type = 'line';
 		this.legend = true;
 		this.labels = ['January', 'February', 'March', 'April'];
-		this.labelsMultipleAxis = [1, 2, 3, 4];
+		this.labelsMultipleAxis = ['1', '2', '3', '4'];
 		this.labelsChartMeterGadget = ['06/05/2020', '06/06/2020', '06/07/2020', '06/08/2020', '06/09/2020', '06/10/2020'];
 
 		this.tooltipSettings.backgroundColor = '#ffffff';
@@ -88,10 +76,6 @@ export class ShowcaseChartComponent {
 		this.tooltipSettings.titleFontColor = '#fd7e14';
 
 		this.generateMultipleAxisExample();
-		this.radialChartGadgetConfiguration = this.generateChartGadgetConfiguration();
-		this.digitalChartGadgetConfiguration = this.generateChartGadgetConfiguration();
-		this.horizontalLinearChartGadgetConfiguration = this.generateChartGadgetConfiguration();
-		this.verticalLinearChartGadgetConfiguration = this.generateChartGadgetConfiguration();
 
 		this.dataLine.push(new ChartItem('Only Line', [13, 20, 21, 15.5], '', '', false, true, false, 3, '',
 			new ChartTooltipItem('title', 'label', 'afterlabel', true)));
@@ -101,56 +85,48 @@ export class ShowcaseChartComponent {
 
 		this.dataTimeLine.push(new ChartItem('Label', [
 			{
-				t: new Date(2021, 5, 15),
+				x: (new Date(2021, 5, 15)),
 				y: 3
 			},
 			{
-				t: new Date(2022, 1, 12),
+				x: (new Date(2022, 1, 12)),
 				y: 1
 			}, {
-				t: new Date(2022, 2, 5),
+				x: (new Date(2022, 2, 5)),
 				y: 10
 			},
 		], '', '', false, true, false, 3, ''));
 		this.dataTimeLine.push(new ChartItem('Label 2', [
 			{
-				t: new Date(2022, 1, 1),
+				x: (new Date(2022, 1, 1)),
 				y: 2
 			},
 			{
-				t: new Date(2022, 1, 20),
+				x: (new Date(2022, 1, 20)),
 				y: 4
 			}, {
-				t: new Date(2022, 2, 14),
+				x: (new Date(2022, 2, 14)),
 				y: 8
 			}, {
-				t: new Date(2022, 3, 7),
+				x: (new Date(2022, 3, 7)),
 				y: 10
 			},
 
 		], '', '', false, true, false, 3, ''));
 
+		this.labelsTimeLine = [
+			new Date(2021, 5, 15),
+			new Date(2022, 1, 12),
+			new Date(2022, 2, 5),
+			new Date(2022, 1, 1),
+			new Date(2022, 1, 20),
+			new Date(2022, 2, 14),
+			new Date(2022, 3, 7),
+		];
+
 
 		this.dataBar.push(new ChartItem('Only Line', [12, 41, 1, 21], '', '', false, false, false, 3));
 		this.dataBar.push(new ChartItem('Line and Area', [13, 20, 21, 15], '', '', true, true, false, 3));
-
-		this.radialDataChartMeterGadget.push(new ChartItem('Goal', [8, 8, 2, 8, 8, 8], 'blue', 'blue', false, true, false,
-			3, 'line', undefined, 5));
-		this.radialDataChartMeterGadget.push(new ChartItem('Value', [5, 1, 2, 1.03, 3, 10], 'green', 'lightgreen', true, true, false, 1));
-
-		this.digitalDataChartMeterGadget.push(new ChartItem('Goal', [8, 8, 2, 8, 8, 8], 'blue', 'blue', false, true, false,
-			3, 'line', undefined, 5));
-		this.digitalDataChartMeterGadget.push(new ChartItem('Value', [0, 1, 2, 1.03, 3, 11], 'green', 'lightgreen', true, true, false, 1));
-		this.digitalDataChartMeterGadget.push(new ChartItem('Value 2', [0, 1, 2, 1.03, 3, 11], 'blue', 'lightblue', true, true, false, 1));
-
-		this.verticalLinearDataChartMeterGadget.push(new ChartItem('Goal', [8, 8, 2, 8, 8, 8], 'blue', 'blue', false, true, false,
-			3, 'line', undefined, 5));
-		this.verticalLinearDataChartMeterGadget.push(new ChartItem('Value', [0, 1, 2, 1.03, 3, 6.6], 'green', 'lightgreen',
-			true, true, false, 1));
-
-		this.horizontalLinearDataChartMeterGadget.push(new ChartItem('Goal', [8, 8, 8, 8], 'blue', 'blue', false, true, false,
-			3, 'line', undefined, 5));
-		this.horizontalLinearDataChartMeterGadget.push(new ChartItem('Value', [3, 1, 12, -10], 'green', 'lightgreen', true, true, false, 1));
 
 		this.dataRadar.push(new ChartItem('Only Line', [36, 41, 35, 21], '', '', false, true, false, 3));
 		this.dataRadar.push(new ChartItem('Line and Area', [37, 40, 21, 15], '', '', true, true, false, 3));
@@ -201,36 +177,36 @@ export class ShowcaseChartComponent {
 		this.dataLineAnnotation.push(new ChartItem('Data Values', [2.4, 2, 1.8, 2.7, 2.5, 2.4, 2.3, 2.8, 2.3, 2.4, 2.7, 2.1, 1.9, 1.8],
 			'', '', false, true, false, 3));
 		this.chartLineAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation(), 2.5,
-			'horizontal', 'beforeDatasetsDraw', 'line', [], '#000000', 1));
-		this.chartLineAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation('-1 SD', 'left', '#e53c29'), 1.5,
-			'horizontal', 'beforeDatasetsDraw', 'line', [5, 15], '#e53c29', 1));
-		this.chartLineAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation('+1 SD', 'left', '#e53c29'), 3.5,
-			'horizontal', 'beforeDatasetsDraw', 'line', [5, 15], '#e53c29', 1));
-		this.chartLineAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation('+2 SD', 'left', '#287ae5'), 4.5,
-			'horizontal', 'beforeDatasetsDraw', 'line', [5, 15], '#287ae5', 1));
-		this.chartLineAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation('-2 SD', 'left', '#287ae5'), 0.5,
-			'horizontal', 'beforeDatasetsDraw', 'line', [5, 15], '#287ae5', 1));
+			'horizontal', 'afterDatasetsDraw', 'line', [], '#000000', 1));
+		this.chartLineAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation('-1 SD', 'start', '#e53c29'), 1.5,
+			'horizontal', 'afterDatasetsDraw', 'line', [5, 15], '#e53c29', 1));
+		this.chartLineAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation('+1 SD', 'start', '#e53c29'), 3.5,
+			'horizontal', 'afterDatasetsDraw', 'line', [5, 15], '#e53c29', 1));
+		this.chartLineAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation('+2 SD', 'start', '#287ae5'), 4.5,
+			'horizontal', 'afterDatasetsDraw', 'line', [5, 15], '#287ae5', 1));
+		this.chartLineAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation('-2 SD', 'start', '#287ae5'), 0.5,
+			'horizontal', 'afterDatasetsDraw', 'line', [5, 15], '#287ae5', 1));
 		for (let g = 1; g <= this.dataLineAnnotation[0].data.length; g++) {
 			this.labelLineAnnotations.push(g);
 		}
 
 		this.chartMultipleAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation(), 4.5,
-			'horizontal', 'beforeDatasetsDraw', 'line', [5, 15], '#287ae5', 1));
+			'horizontal', 'afterDatasetsDraw', 'line', [5, 15], '#287ae5', 1));
 		this.chartMultipleAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation(), 9,
-			'horizontal', 'beforeDatasetsDraw', 'line', [5, 15], '#287ae5', 1));
-		this.chartMultipleAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation('Label', 'left', '#287ae5'), -1,
-			'horizontal', 'beforeDatasetsDraw', 'line', [5, 15], '#287ae5', 1));
+			'horizontal', 'afterDatasetsDraw', 'line', [5, 15], '#287ae5', 1));
+		this.chartMultipleAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation('Label', 'start', '#287ae5'), -1,
+			'horizontal', 'afterDatasetsDraw', 'line', [5, 15], '#287ae5', 1));
 		this.chartMultipleAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation(), 6,
-			'vertical', 'beforeDatasetsDraw', 'line', [], '#42f483', 1));
+			'vertical', 'afterDatasetsDraw', 'line', [], '#42f483', 1));
 
 		/*Diagonal Annotation */
 		this.chartMultipleAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation(), -5,
-			'horizontal', 'beforeDatasetsDraw', 'line', [], '#000', 1, 20));
-		this.chartMultipleAnnotations.push(new ChartBoxAnnotation('beforeDatasetsDraw', 2, 10, 2, 10, 'box', '', '#cccccc'));
-		this.chartMultipleAnnotations.push(new ChartBoxAnnotation('beforeDatasetsDraw', 0, 12, 0, 12, 'box', '', '#5ac14b'));
+			'horizontal', 'afterDatasetsDraw', 'line', [], '#000', 1, 20));
+		this.chartMultipleAnnotations.push(new ChartBoxAnnotation('afterDatasetsDraw', 2, 10, 2, 10, 'box', '', '#cccccc'));
+		this.chartMultipleAnnotations.push(new ChartBoxAnnotation('afterDatasetsDraw', 0, 12, 0, 12, 'box', '', '#5ac14b'));
 
-		this.chartBubbleAnnotations.push(new ChartBoxAnnotation('beforeDatasetsDraw', 2, 10, 2, 10, 'box', '', '#cccccc'));
-		this.chartBubbleAnnotations.push(new ChartBoxAnnotation('beforeDatasetsDraw', 0, 12, 0, 12, 'box', '', '#5ac14b'));
+		this.chartBubbleAnnotations.push(new ChartBoxAnnotation('afterDatasetsDraw', 2, 10, 2, 10, 'box', '', '#cccccc'));
+		this.chartBubbleAnnotations.push(new ChartBoxAnnotation('afterDatasetsDraw', 0, 12, 0, 12, 'box', '', '#5ac14b'));
 
 		this.dataBubbleAnnotations.push(new ChartItem('Test 1', [{x: 13, y: 13, r: 2}, {x: 1, y: 2, r: 2}, {x: 15, y: 23, r: 2},
 			{x: -2, y: -2, r: 2}, {x: -10, y: 13, r: 2}, {x: 23, y: 12, r: 2}, {x: 4, y: 4, r: 2}, {x: 5, y: 6, r: 2},
@@ -243,14 +219,15 @@ export class ShowcaseChartComponent {
 		this.dataLineCustomLegend.push(new ChartItem('Line and Area', [12, 41, 1, 21], '', '', true, true,
 			false, 3, undefined, undefined, undefined, undefined, 'bar'));
 
-			this.chartLine = new ChartLine(0.5, 5, 2.5, 20, null, null);
+		this.chartLine = new ChartLine(0.5, 5, 2.5, 20, null, null);
 
 	}
 
-	public doAction(event: any) {
-		const xValue = this.labels[this.itemSelected._index];
-		const yValue = this.dataPie[0].data[this.itemSelected._index];
+	private static randomIntFromInterval(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
 	}
+
+	public doAction(event: any) {}
 
 	public doChange() {
 		this.dataLine = [];
@@ -265,6 +242,7 @@ export class ShowcaseChartComponent {
 				fill = true;
 			}
 			this.dataLine.push(new ChartItem('Line ' + h, dataRnd, '', '', fill, true, false, 3));
+			console.log({rnd, dataRnd});
 		}
 		this.lineChart.doUpdate();
 	}
@@ -275,7 +253,7 @@ export class ShowcaseChartComponent {
 		for (let h = 1; h <= rnd; h++) {
 			const dataRnd: Array<any> = [];
 			for (let i = 1; i <= 4; i++) {
-				let item = {
+				const item = {
 					t: new Date(ShowcaseChartComponent.randomIntFromInterval(2020, 2022),
 						ShowcaseChartComponent.randomIntFromInterval(1, 11),
 						ShowcaseChartComponent.randomIntFromInterval(1, 20)),
@@ -297,23 +275,6 @@ export class ShowcaseChartComponent {
 		this.lineChartMultipleAxis.doUpdate();
 	}
 
-	public generateChartGadgetConfiguration(): ChartMeterConfiguration {
-		const chartMeterConfiguration = new ChartMeterConfiguration();
-		chartMeterConfiguration.numberFormat = '#.####';
-		chartMeterConfiguration.betterValues = 'higher';
-		chartMeterConfiguration.chartColour = 'green';
-		chartMeterConfiguration.goalColour = 'blue';
-		chartMeterConfiguration.defaultGoalValue = 8;
-		chartMeterConfiguration.minVisualValue = -2213124102.2132321;
-		chartMeterConfiguration.maxVisualValue = 123456789.2903232;
-		chartMeterConfiguration.levels.push({levelColor: '#ff0000', minValue: 0, maxValue: 2});
-		chartMeterConfiguration.levels.push({levelColor: '#FFFF00AA', minValue: 3, maxValue: 5});
-		chartMeterConfiguration.levels.push({levelColor: '#00FF00AA', minValue: 6, maxValue: 10});
-		chartMeterConfiguration.showHistory = false;
-		chartMeterConfiguration.markerForGoal = 'rectRot';
-		return chartMeterConfiguration;
-	}
-
 	public generateMultipleAxisExample(): void {
 		this.dataLineMultipleAxis = [];
 
@@ -322,11 +283,21 @@ export class ShowcaseChartComponent {
 		this.multipleYAxisScales.push(this.generateChartMultipleYAxisScales('y-axis-1', 'right', 0, 10, 2));
 
 		this.chartLineMultipleAxisAnnotations.push(new ChartLineAnnotation(new ChartLabelAnnotation('Label', 'top'), 3,
-			'vertical', 'beforeDatasetsDraw', 'line', [], '#e53c29'));
+			'vertical', 'afterDatasetsDraw', 'line', [], '#e53c29'));
 
 		const rndData = ShowcaseChartComponent.randomIntFromInterval(1, 3);
 		this.generateRandomData(rndData, 30, 105, 'y-axis-0', 'left');
 		this.generateRandomData(rndData, 0, 10, 'y-axis-1', 'right');
+	}
+
+	public doResizeChart() {
+		this.resizedImage = this.resizableChart.getResizedBase64Image(this.defaultResizeHeight, this.defaultResizeWidth);
+	}
+
+	public doUpdateChart() {
+		this.resizableChart.chartResized = false;
+		this.resizableChart.doUpdate();
+		this.resizableChart.chart.resize();
 	}
 
 	private generateRandomData(rndNumberOfElements: number, min: number, max: number, yAxisID: string, position: string) {
@@ -339,7 +310,7 @@ export class ShowcaseChartComponent {
 			if (ShowcaseChartComponent.randomIntFromInterval(0, 1) === 1) {
 				fill = true;
 			}
-			this.dataLineMultipleAxis.push(new ChartItem('Line ' + h + ' ' + position + ' axis', dataRnd, '', '', fill, true, false, 3,
+			this.dataLineMultipleAxis.push(new ChartItem(`Line ${h} ${position} axis`, dataRnd, '', '', fill, true, false, 3,
 				'line', undefined, ShowcaseChartComponent.randomIntFromInterval(0, 3), yAxisID));
 		}
 	}
@@ -354,10 +325,10 @@ export class ShowcaseChartComponent {
 		defaultYAxisScales.scaleLabel = {display: true, labelString: this.yLabelAxis};
 
 		defaultYAxisScales.ticks = {
-			min:      min,
-			max:      max,
 			display:  true,
-			stepSize: stepSize
+			min,
+			max,
+			stepSize,
 		};
 
 		return defaultYAxisScales;
@@ -383,82 +354,6 @@ export class ShowcaseChartComponent {
 				return [0, 127, 200];
 			default:
 				return [255, 228, 181];
-		}
-	}
-
-	public doShowHistory(meterType: string): void {
-		switch (meterType) {
-			case 'radialMeter':
-				this.radialChartGadgetConfiguration.showHistory = !this.radialChartGadgetConfiguration.showHistory;
-				this.radialMeterChart.doUpdate();
-				break;
-			case 'digitalMeter':
-				this.digitalChartGadgetConfiguration.showHistory = !this.digitalChartGadgetConfiguration.showHistory;
-				this.digitalMeterChart.doUpdate();
-				break;
-			case 'horizontalLinearMeter':
-				this.horizontalLinearChartGadgetConfiguration.showHistory = !this.horizontalLinearChartGadgetConfiguration.showHistory;
-				this.horizontalLinearMeterChart.doUpdate();
-				break;
-			case 'verticalLinearMeter':
-				this.verticalLinearChartGadgetConfiguration.showHistory = !this.verticalLinearChartGadgetConfiguration.showHistory;
-				this.verticalLinearMeterChart.doUpdate();
-				break;
-			default:
-				break;
-		}
-	}
-
-	public doResizeChart() {
-		this.resizedImage = this.resizableChart.getResizedBase64Image(this.defaultResizeHeight, this.defaultResizeWidth);
-	}
-
-	public doUpdateChart() {
-		this.resizableChart.chartResized = false;
-		this.resizableChart.doUpdate();
-		this.resizableChart.chart.resize();
-	}
-
-	public getVisualValue(chartMeterConfiguration: ChartMeterConfiguration, chartItems: Array<ChartItem>, minValue = true): number {
-		let currentValue: number;
-
-		if (minValue) {
-			currentValue = Number.POSITIVE_INFINITY;
-			if (chartItems) {
-				chartItems.forEach(chartItem => currentValue = Math.min(currentValue, ...chartItem.data));
-			}
-
-			if (currentValue === Number.POSITIVE_INFINITY) {
-				return chartMeterConfiguration.minVisualValue;
-			} else if (!chartMeterConfiguration.minVisualValue) {
-				return currentValue;
-			} else {
-				return Math.min(chartMeterConfiguration.minVisualValue, currentValue);
-			}
-		} else {
-
-			const arrayOfValues: Array<number> = [Number.NEGATIVE_INFINITY];
-			if (chartItems) {
-				const maxLength = Math.max(...chartItems.map(item => item.data.length));
-				for (let index = 0; index < maxLength; index++) {
-					let value = 0;
-					chartItems.forEach(item => {
-						if (item.data.length > index) {
-							value += item.data[index];
-						}
-					});
-					arrayOfValues.push(value);
-				}
-				currentValue = Math.max(...arrayOfValues);
-
-				if (currentValue === Number.NEGATIVE_INFINITY) {
-					return chartMeterConfiguration.maxVisualValue;
-				} else if (chartMeterConfiguration.maxVisualValue && chartMeterConfiguration.maxVisualValue > currentValue) {
-					return chartMeterConfiguration.maxVisualValue;
-				} else {
-					return undefined;
-				}
-			}
 		}
 	}
 }
