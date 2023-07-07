f6e01cb18749ca4c9382ba64b08a7241f0048fea
Joaquim Vila Bou <jvila2@werfen.com>
2023-07-07 12:30:32 +0200

    Added flag to detect when ChartJS is "drawing" on the canvas (required for e2e testing)

--- projects/systelab-charts/src/lib/chart/chart.component.ts
+++ projects/systelab-charts/src/lib/chart/chart.component.ts
@@ -1,21 +1,33 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterContentInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, InteractionMode } from './interfaces';
 import * as ChartJS from 'chart.js';
 import { ChartService } from './services/chart.service';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import annotationPlugin from 'chartjs-plugin-annotation';
 
-ChartJS.Chart.register(...ChartJS.registerables, ChartDataLabels, annotationPlugin);
+const afterRenderingDetectorPlugin = {
+  id: 'afterRenderingDetector',
+  afterInit: (chart, args, options) => options.afterInitCallback(),
+  afterUpdate: (chart, args, options) => options.afterUpdateCallback(),
+  defaults: {
+    afterInitCallback: () => {},
+    afterUpdateCallback: () => {},
+  }
+};
+
+ChartJS.Chart.register(...ChartJS.registerables, ChartDataLabels, annotationPlugin, afterRenderingDetectorPlugin);
+
 
 @Component({
   selector: 'systelab-chart',
   templateUrl: './chart.component.html',
 })
-export class ChartComponent implements OnInit, AfterViewInit {
+export class ChartComponent implements OnInit, AfterContentInit {
   @Input() config: ChartConfiguration;
   @ViewChild('chartCanvas', {static: true}) chartCanvas: ElementRef;
 
   public chart: ChartJS.Chart;
+  public drawing = true;
 
   constructor(private readonly chartService: ChartService) {}
 
@@ -24,15 +36,51 @@ export class ChartComponent implements OnInit, AfterViewInit {
     ChartJS.Chart.defaults.interaction.mode = this.config.options?.interaction?.mode ?? InteractionMode.index;
   }
 
-  public ngAfterViewInit(): void {
-    if (this.chartCanvas.nativeElement) {
-      const cx: CanvasRenderingContext2D = this.chartCanvas.nativeElement.getContext('2d');
-      const chartConfiguration: ChartJS.ChartConfiguration = this.chartService.mapConfiguration(this.config, cx);
-      this.drawChart(chartConfiguration, cx);
-    }
+  public ngAfterContentInit(): void {
+    this.drawChart();
   }
 
-  private drawChart(chartConfiguration: ChartJS.ChartConfiguration, cx: CanvasRenderingContext2D): void {
+  private drawChart(): void {
+    if (!this.chartCanvas.nativeElement) {
+      return;
+    }
+
+    this.drawing = true;
+
+    const cx: CanvasRenderingContext2D = this.chartCanvas.nativeElement.getContext('2d');
+    let chartConfiguration: ChartJS.ChartConfiguration = this.chartService.mapConfiguration(this.config, cx);
+    const animationEnabled: boolean = (chartConfiguration.options.animation as any).duration > 0;
+
+    chartConfiguration = {
+      ...chartConfiguration,
+      options: {
+        ...chartConfiguration.options,
+        animation: {
+          ...chartConfiguration.options.animation,
+          onComplete: () => {
+            this.drawing = false;
+            console.log('animation callback');
+          },
+        },
+        plugins: {
+          ...chartConfiguration.options.plugins,
+          afterRenderingDetector: {
+            afterInitCallback: () => {
+              if (!animationEnabled) {
+                this.drawing = false;
+              }
+            },
+            afterUpdateCallback: () => {
+              if (!animationEnabled) {
+                this.drawing = false;
+              }
+            },
+          },
+        } as any,
+      }
+    };
+    console.log(chartConfiguration);
+
     this.chart = new ChartJS.Chart(cx, chartConfiguration);
   }
 }
