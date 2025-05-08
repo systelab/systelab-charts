8781c57086ed084ea872a74c37f69c5799b80097
rmoya <rmoya@werfen.com>
2025-05-08 10:28:46 +0200

    Added new type in pointStyle param. Added onClick function/param in Chartconfiguration interface.
    ScatterChart example modified to show the changes

--- projects/showcase/src/app/components/showcase-chart/chart-types/scatter-chart.ts
+++ projects/showcase/src/app/components/showcase-chart/chart-types/scatter-chart.ts
@@ -1,4 +1,9 @@
-import { ChartConfiguration, ChartType } from 'systelab-charts';
+import { ChartConfiguration, ChartType, InteractionMode, ScatterPoint } from 'systelab-charts';
+import { ActiveElement, Chart, ChartEvent } from 'chart.js';
+
+//Custom image to be displayed in all points of the chart - this is just an example
+let checkMark: HTMLImageElement = new Image(16, 16);
+checkMark.src = 'https://banner2.cleanpng.com/20180703/oxt/aaxcwnq10.webp';
 
 export const scatterChartConfiguration: ChartConfiguration = {
     type: ChartType.scatter,
@@ -25,4 +30,29 @@ export const scatterChartConfiguration: ChartConfiguration = {
             showLine: false,
         },
     ],
+    legend: {
+        labels: {
+            enabled: true,
+            pointStyle: checkMark
+        }
+    },
+    options: {
+        responsive: true,
+        interaction: {
+            intersect: false,
+            mode: InteractionMode.point,
+        },
+        onClick: (event: ChartEvent, elements: ActiveElement[], chart: Chart): ScatterPoint => {
+            // Example to receive a ScatterPoint coordinates when clicking on the point
+            const canvasPosition: ScatterPoint = {x: event.x, y: event.y}
+
+            const dataX: string = chart.scales.x.getValueForPixel(canvasPosition.x).toFixed(2);
+            const dataY: string = chart.scales.y.getValueForPixel(canvasPosition.y).toFixed(2);
+
+            return {
+                x: parseFloat(dataX),
+                y: parseFloat(dataY)
+            }
+        }
+    }
 };
